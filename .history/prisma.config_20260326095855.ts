4dac62625c2e9cb20ac10198a1cb727db9fad68d
komahan jeyaseelan <jeyaseelankomakan@gmail.com>
2026-03-26 11:08:29 +0530

    final

--- .history/prisma.config_20260326095855.ts
+++
@@ -1,9 +0,0 @@
-import { defineConfig } from "prisma/config";
-
-export default defineConfig({
-  schema: "./prisma/schema.prisma",
-  // @ts-ignore - datasource.url required for prisma db push in v7
-  datasource: {
-    url: process.env.DATABASE_URL as string,
-  },
-});
